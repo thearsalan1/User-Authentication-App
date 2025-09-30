21687870dcb9e4cecefc97f2f08fa29a63c6699b
thearsalan1 <arsalanloq@gmail.com>
2025-09-30 14:15:18 +0530

    Initial code base of frontend created

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import EmailVerify from "./Pages/EmailVerify";
+import ResetPassword from "./Pages/ResetPassword";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/email-verify" element={<EmailVerify />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+    </Routes>
+  );
+};
+
+export default App;
